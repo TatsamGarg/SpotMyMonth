55ded7e6e385c410252225cffe32fb9323ed426d
Tatsam Garg <125929067+TatsamGarg@users.noreply.github.com>
2023-02-22 15:34:44 +0100

    Update SpotMyMonth_opensource.py

--- SpotMyMonth_opensource.py
+++ SpotMyMonth_opensource.py
@@ -68,6 +68,80 @@ class PlaylistGeneratorGUI:
 
         if self.generate_button:
             self.generate_playlists()
+            @self.app.route('/callback')    
+    def callback(self):
+        # Extract the code from the redirect URI
+        code = request.args.get('code')
+
+        # Use the code to get an access token
+        token_info = auth_manager.get_access_token(code=code)
+
+        # Use the access token to make API requests
+        sp = spotipy.Spotify(auth_manager=auth_manager)
+
+        # Do something with the Spotify API
+
+        # get the start and end months from the inputs
+        start_month_str = self.start_month_input
+        end_month_str = self.end_month_input
+
+        # convert the start and end months to datetime objects
+        start_month = datetime.strptime(start_month_str, "%m/%Y")
+        end_month = datetime.strptime(end_month_str, "%m/%Y")
+        days_in_month = calendar.monthrange(end_month.year, end_month.month)[1]
+        end_month += timedelta(days=days_in_month)
+
+        # get all tracks from your 'liked' songs library
+        offset = 0
+        liked_tracks = []
+        while True:
+            results = sp.current_user_saved_tracks(offset=offset)
+            liked_tracks.extend(results['items'])
+            offset += len(results['items'])
+            if len(results['items']) == 0:
+                break
+
+        # group the 'liked' songs by month
+        tracks_by_month = {}
+        for item in liked_tracks:
+            added_at = datetime.strptime(item['added_at'], '%Y-%m-%dT%H:%M:%SZ')
+            month = datetime(added_at.year, added_at.month, 1)
+
+            if start_month <= month < end_month:
+                month_str = datetime.strftime(month, '%b%y')
+                if month_str in tracks_by_month:
+                    tracks_by_month[month_str].append(item['track']['uri'])
+                else:
+                    tracks_by_month[month_str] = [item['track']['uri']]
+
+        # create a new playlist for each month
+        for month, track_uris in tracks_by_month.items():
+            playlist_name = f"{month}"
+
+            # check if playlist already exists for this month
+            playlists = sp.current_user_playlists()
+            for playlist in playlists['items']:
+                if playlist['name'] == playlist_name:
+                    existing_playlist = playlist
+                    break
+            else:
+                existing_playlist = None
+
+            # create or update the playlist
+            if existing_playlist:
+                existing_playlist_tracks = [track['track'] for track in sp.playlist_tracks(existing_playlist['id'])['items']]
+                existing_track_uris = [track['uri'] for track in existing_playlist_tracks]
+                new_track_uris = list(set(track_uris) - set(existing_track_uris))
+
+                if len(new_track_uris)==0:
+                    st.write(f"Playlist '{playlist_name}' already exists and was updated with {len(new_track_uris)} tracks!")
+                else :
+                    sp.user_playlist_add_tracks(user=sp.me()['id'], playlist_id=existing_playlist['id'], tracks=new_track_uris)
+                    st.write(f"Playlist '{playlist_name}' already exists and was updated with {len(new_track_uris)} tracks!")
+            else:
+                playlist = sp.user_playlist_create(user=sp.me()['id'], name=playlist_name)
+                sp.user_playlist_add_tracks(user=sp.me()['id'], playlist_id=playlist['id'], tracks=track_uris)
+                st.write(f"Playlist '{playlist_name}' created with {len(track_uris)} tracks!") 
 
     def generate_playlists(self):
         # Set up client credentials
@@ -87,82 +161,7 @@ class PlaylistGeneratorGUI:
         # Redirect the user to the Spotify authorization page
         auth_url = auth_manager.get_authorize_url()
         st.write(auth_url)
-        
-        @self.app.route('/callback')    
-        def callback(self):
-            # Extract the code from the redirect URI
-            code = request.args.get('code')
-
-            # Use the code to get an access token
-            token_info = auth_manager.get_access_token(code=code)
-
-            # Use the access token to make API requests
-            sp = spotipy.Spotify(auth_manager=auth_manager)
-
-            # Do something with the Spotify API
-
-            # get the start and end months from the inputs
-            start_month_str = self.start_month_input
-            end_month_str = self.end_month_input
-
-            # convert the start and end months to datetime objects
-            start_month = datetime.strptime(start_month_str, "%m/%Y")
-            end_month = datetime.strptime(end_month_str, "%m/%Y")
-            days_in_month = calendar.monthrange(end_month.year, end_month.month)[1]
-            end_month += timedelta(days=days_in_month)
-
-            # get all tracks from your 'liked' songs library
-            offset = 0
-            liked_tracks = []
-            while True:
-                results = sp.current_user_saved_tracks(offset=offset)
-                liked_tracks.extend(results['items'])
-                offset += len(results['items'])
-                if len(results['items']) == 0:
-                    break
-
-            # group the 'liked' songs by month
-            tracks_by_month = {}
-            for item in liked_tracks:
-                added_at = datetime.strptime(item['added_at'], '%Y-%m-%dT%H:%M:%SZ')
-                month = datetime(added_at.year, added_at.month, 1)
-
-                if start_month <= month < end_month:
-                    month_str = datetime.strftime(month, '%b%y')
-                    if month_str in tracks_by_month:
-                        tracks_by_month[month_str].append(item['track']['uri'])
-                    else:
-                        tracks_by_month[month_str] = [item['track']['uri']]
-
-            # create a new playlist for each month
-            for month, track_uris in tracks_by_month.items():
-                playlist_name = f"{month}"
-
-                # check if playlist already exists for this month
-                playlists = sp.current_user_playlists()
-                for playlist in playlists['items']:
-                    if playlist['name'] == playlist_name:
-                        existing_playlist = playlist
-                        break
-                else:
-                    existing_playlist = None
-
-                # create or update the playlist
-                if existing_playlist:
-                    existing_playlist_tracks = [track['track'] for track in sp.playlist_tracks(existing_playlist['id'])['items']]
-                    existing_track_uris = [track['uri'] for track in existing_playlist_tracks]
-                    new_track_uris = list(set(track_uris) - set(existing_track_uris))
-
-                    if len(new_track_uris)==0:
-                        st.write(f"Playlist '{playlist_name}' already exists and was updated with {len(new_track_uris)} tracks!")
-                    else :
-                        sp.user_playlist_add_tracks(user=sp.me()['id'], playlist_id=existing_playlist['id'], tracks=new_track_uris)
-                        st.write(f"Playlist '{playlist_name}' already exists and was updated with {len(new_track_uris)} tracks!")
-                else:
-                    playlist = sp.user_playlist_create(user=sp.me()['id'], name=playlist_name)
-                    sp.user_playlist_add_tracks(user=sp.me()['id'], playlist_id=playlist['id'], tracks=track_uris)
-                    st.write(f"Playlist '{playlist_name}' created with {len(track_uris)} tracks!") 
-
+       
 if __name__ == "__main__":
     playlist_generator_gui = PlaylistGeneratorGUI()
     playlist_generator_gui.run()
